0998e499282cb233efa79a4e4e8df2cc8b7e3b4c
baikangwang <baikangwang@hotmail.com>
2016-12-06 11:37:55 +0800

    update the company and product field in the assembly info

--- Aeronet.Chart/Properties/AssemblyInfo.cs
+++ Aeronet.Chart/Properties/AssemblyInfo.cs
@@ -7,10 +7,10 @@ using System.Resources;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Aeronet Chart")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Alpha edition")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Aeronet Chart")]
+[assembly: AssemblyCompany("北京宇星惠通科技有限公司")]
+[assembly: AssemblyProduct("CIMEL光度计数据处理软件")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -33,6 +33,6 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
--- Aeronet.Splitter/Properties/AssemblyInfo.cs
+++ Aeronet.Splitter/Properties/AssemblyInfo.cs
@@ -8,8 +8,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Aeronet.Splitter")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Aeronet.Splitter")]
+[assembly: AssemblyCompany("北京宇星惠通科技有限公司")]
+[assembly: AssemblyProduct("CIMEL光度计数据处理软件")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
