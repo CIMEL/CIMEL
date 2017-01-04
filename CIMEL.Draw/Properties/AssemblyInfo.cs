970e5a11611f0b1b437ba2fe087df9ea73aeb66d
baikangwang <baikangwang@hotmail.com>
2017-01-04 11:42:14 +0800

    change neutral language to Chinese

--- CIMEL.Draw/Properties/AssemblyInfo.cs
+++ CIMEL.Draw/Properties/AssemblyInfo.cs
@@ -35,4 +35,4 @@ using System.Resources;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1.0.17003")]
 [assembly: AssemblyFileVersion("1.1.0.17003")]
-[assembly: NeutralResourcesLanguageAttribute("en-US")]
+[assembly: NeutralResourcesLanguageAttribute("zh-CN")]
