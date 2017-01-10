ce22c9aacf08093f03eaffb3474ea4cfb19036de
Baker Wang <baikangwang@hotmail.com>
2017-01-10 15:31:56 +0800

    Fxied #42

---
+++ CIMEL.Chart/SplashScreen.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CIMEL.Chart
+{
+    public partial class SplashScreen : Form
+    {
+        public SplashScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.progressBar1.Increment(1);
+            if(this.progressBar1.Value==100) this.timer1.Stop();
+        }
+    }
+}
