1d4196c47665512561508cf1d0c88646c2a7630a
baikangwang <baikangwang@hotmail.com>
2018-07-07 00:28:04 +0800

    change brand

--- CIMEL.Chart/Properties/AssemblyInfo.cs
+++ CIMEL.Chart/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Resources;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CIMEL Data & Chart")]
-[assembly: AssemblyDescription("CIMEL太阳光度计数据处理软件")]
+[assembly: AssemblyTitle("大气气溶胶光学参数处理软件")]
+[assembly: AssemblyDescription("适用于CIMEL太阳光度计数据处理")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("北京宇星惠通科技有限公司")]
-[assembly: AssemblyProduct("CIMEL太阳光度计数据处理软件")]
+[assembly: AssemblyCompany("天津澳远风云科技有限公司")]
+[assembly: AssemblyProduct("大气气溶胶光学参数处理软件")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -33,6 +33,6 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.17121")]
-[assembly: AssemblyFileVersion("1.1.0.17121")]
+[assembly: AssemblyVersion("1.2.0.18187")]
+[assembly: AssemblyFileVersion("1.2.0.18187")]
 [assembly: NeutralResourcesLanguageAttribute("zh-CN")]
