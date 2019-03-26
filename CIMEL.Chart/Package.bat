e6b49a667e4fa90cf1ebf5f57d8e0706d3cfe77f
baikangwang <baikangwang@hotmail.com>
2019-03-27 01:08:01 +0800

    update fortran program

--- CIMEL.Chart/Package.bat
+++ CIMEL.Chart/Package.bat
@@ -7,3 +7,5 @@ set dest=%base_dir%\CIMEL\..\CIMELPackage\
 XCOPY /Y "%output%CIMELData" "%dest%CIMELData"
 XCOPY /Y "%output%Options" "%dest%Options"
 XCOPY /Y "%output%大气气溶胶光学参数处理软件.exe" "%dest%"
+
+pause;
